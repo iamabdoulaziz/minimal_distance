f43b1f164eee5e5e64e0f859dd077d8dd52a307e
Cute Boy <abdoulazizc867@gmail.com>
2024-12-05 00:42:59 +0000

    This is the correction of the exercise

--- minamal_distance.py
+++ minamal_distance.py
@@ -3,6 +3,7 @@
 drivers_name = ["Sycasso", "Bamby", "Cute boy", "Alienne", "Super sayenne", "Z-hero", "Hero"]
 driver_distance_km = [1.5, 2.2, 0.4, 0.9, 7.1, 1.1, 0.6]
 
+"""
 min_distance = driver_distance_km[0]
 distance_index = 0
 for distance in driver_distance_km:
@@ -14,3 +15,17 @@ for distance in driver_distance_km:
 
 print(f"La distance minimale est : {min_distance} km")
 print(f"L'index est : {distance_index}")
+"""
+driver = drivers_name[0]
+min_index = 0
+min_distance = driver_distance_km[0]
+for i in range(len(driver_distance_km)):
+    distance = driver_distance_km[i]
+    if distance < min_distance:
+        min_distance = distance
+        min_index = i
+        driver = drivers_name[i]
+
+print(f" Distance minimal {min_distance} km")
+print(f"Index de la distance minimale : {min_index}")
+print(f"Le nom du chauffeur est : {driver}")
